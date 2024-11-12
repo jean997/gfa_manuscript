569bfb5288ce654fc014cd28911574f3ded303ec
Jean Morrison <jvmorr@gl-login1.arc-ts.umich.edu>
2024-11-12 16:07:57 -0500

    update simulation code

--- code/simulations/R/ml.R
+++ code/simulations/R/ml.R
@@ -1,21 +1,40 @@
+library(nFactors)
 
 dat <- readRDS(snakemake@input[["inp"]])
 R <- readRDS(snakemake@input[["R"]])
-nfact <- as.numeric(snakemake@wildcards[["nfct"]])
-Rg <- R$R_ldsc$Rg
+cond_num <- as.numeric(snakemake@wildcards[["cond_num"]])
 
-f <- try(stats::factanal(covmat = Rg, factors = nfact, rotation="promax"), silent = TRUE);
-if(inherits(f, "try-error")){
+#nfact <- as.numeric(snakemake@wildcards[["nfct"]])
+Rg <- GFA::condition(R$R_ldsc$Rg, cond_num = cond_num, corr = TRUE)
+
+eR <- eigen(Rg, only.values=T)$values
+nF <- nFactors::nScree(x = eR)$Components |> as.list()
+i <- which(names(nF) == "nparallel")
+nF <- nF[-i]
+nF$six <- 6
+nF$twelve <- 12
+
+fits <- lapply(nF, function(nfact){
+    if(nfact == 0){
+        fit <- list("F_hat" = matrix(NA, nrow = ncol(Z_hat), ncol = 0), 
+                    "L_hat" = NULL,
+                     "ix" = ix) 
+        return(fit)
+    }
+
+    f <- try(stats::factanal(covmat = Rg, factors = nfact, rotation="promax"), silent = TRUE);
+    if(inherits(f, "try-error")){
         fit <- list("F_hat" = NULL,
                   "L_hat" = NULL, 
                   "B_hat" = NULL, 
                   "fit" = f);
-}else{
+    }else{
         fit <- list("F_hat" = matrix(f$loadings, nrow = nrow(Rg), ncol = nfact), 
                   "L_hat" = NULL, 
-                  "B_hat" = NULL, 
                   "fit" = f);
-}
+    }
+    fit
+    })
 
-saveRDS(fit, file = snakemake@output[["out"]])
+saveRDS(fits, file = snakemake@output[["out"]])
 
