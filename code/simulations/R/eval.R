776ee97ea0f7a1b63a184613e547d4c61462d164
Jean Morrison <jvmorr@umich.edu>
2025-09-03 02:39:20 -0400

    update eval.R

--- code/simulations/R/eval.R
+++ code/simulations/R/eval.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(GFA)
 
-eval <- function(fit, dat, n_subset = NULL){
+eval <- function(fit, f_true, n_subset = NULL){
     disc_thresh = c(0.9, 0.95, 0.98);
     extra_thresh = c(0.8, 0.7);
 
@@ -10,19 +10,9 @@ eval <- function(fit, dat, n_subset = NULL){
             if(!is.null(fit$F_hat)){
                 fit$F_hat <- fit$F_hat[, c(), drop = F]
             }
-            if(!is.null(fit$L_hat)){
-                fit$L_hat <- fit$L_hat[, c(), drop = F]
-            }
-            fit$B_hat <- NULL
         }else{
             if(!is.null(fit$F_hat)){
                 fit$F_hat <- fit$F_hat[, 1:n_subset, drop = F]
-            }
-            if(!is.null(fit$L_hat)){
-                fit$L_hat <- fit$L_hat[, 1:n_subset, drop = F]
-            }
-            if(!is.null(fit$F_hat) & !is.null(fit$L_hat)){
-                fit$B_hat <- fit$L_hat %*% t(fit$F_hat)
             }
         }
     }
@@ -32,55 +22,19 @@ eval <- function(fit, dat, n_subset = NULL){
         n_disc <- rep(NA, length(disc_thresh));
         n_extra <- rep(NA, length(extra_thresh));
         frob_n <- NA 
-        #frob_n_l <- NA
-        #err <- NA;
-        n_hat <- 0
+        n_total <- NA 
     }else{
-        if(!"matrix" %in% class(fit$F_hat)){
+        if(!inherits(fit$F_hat, "matrix")){
             fit$F_hat <- matrix(fit$F_hat, ncol=1)
-            if(!is.null(fit$L_hat)) fit$L_hat <- matrix(fit$L_hat, ncol=1);
         }
-        if(is.null(fit$L_hat)){
-            err <- NA;
-            sol <- min_norm(f_true = dat$F_mat, f_hat = fit$F_hat);
-            multi_ix <- sol$solution %>% filter(!is.na(est_ix) & !is.na(val)) %>% pull(est_ix)
-            n_hat <- length(multi_ix)
-            #frob_n_l <- NA;
-        }else{
-            sol <- min_norm(f_true = dat$F_mat, f_hat = fit$F_hat,
-                                l_true = dat$L_mat_marg[fit$ix,],
-                                l_hat = fit$L_hat);
-            if(is.null(sol$solution)){
-                multi_ix <- NULL
-                n_hat <- 0
-            }else{
-                multi_ix <- sol$solution %>% filter(!is.na(est_ix) & !is.na(val)) %>% pull(est_ix)
-                n_hat <- length(multi_ix)
-            }
-            #frob_n_l <- sol$frob_n_l
-
-            #ix <- fit$ix;
-            #Bmulti <- dat$beta_marg[ix,] - dat$theta_marg[ix,];
-            #Ball <- dat$beta_marg[ix,]
-            #if(is.null(fit$scale)){
-            #    B_hat <- (fit$L_hat %*% t(fit$F_hat))*dat$s_estimate[ix,];
-            #}else{
-            #    B_hat <- (fit$L_hat %*% t(fit$F_hat*fit$scale))*dat$s_estimate[ix,];
-            #}
-            #err_all <- sqrt(sum((B_hat-Ball)^2)/sum(Ball^2));
-    
-            #if(length(multi_ix) > 0){
-            #    if(is.null(fit$scale)){
-            #        B_hatm <- (fit$L_hat[,multi_ix] %*% t(fit$F_hat[,multi_ix]))*dat$s_estimate[ix,];
-            #    }else{
-            #        B_hatm <- (fit$L_hat[,multi_ix] %*% t(fit$F_hat[,multi_ix]*fit$scale))*dat$s_estimate[ix,];
-            #    }
-            #    err_multi <- sqrt(sum((B_hatm-Bmulti)^2)/sum(Bmulti^2));
-            #}
-
+        if(is.null(f_true)){
+            f_true <- diag(nrow(fit$F_hat))
         }
-        n_disc <- sapply(disc_thresh, function(x){sum(sol$solution$val > x, na.rm=T)});
-        n_extra <- sapply(extra_thresh, function(x){sum(sol$solution$val < x & !is.na(sol$solution$est_ix), na.rm=T)});
+        sol <- min_norm(f_true = f_true, f_hat = fit$F_hat, single_trait_thresh = 0.98)
+        
+        n_disc <- sapply(disc_thresh, function(x){sum(sol$solution$match_score > x, na.rm=T)});
+        n_extra <- sapply(extra_thresh, function(x){sum(sol$solution$match_score < x & !is.na(sol$solution$est_ix), na.rm=T)});
+        n_total <- sum(!is.na(sol$solution$est_ix))
         frob_n <- sol$frob_n
     }
     res <- data.frame(frob_n = frob_n, 
@@ -88,44 +42,40 @@ eval <- function(fit, dat, n_subset = NULL){
                       n_extra_0.7 = n_extra[2],
                       n_disc_0.9  = n_disc[1],                 
                       n_disc_0.95  = n_disc[2],
-                      n_disc_0.98   = n_disc[3],
-                      n_multi_factors = n_hat)
+                      n_disc_0.98   = n_disc[3], 
+                      n_total = n_total)
     return(res)
 }
 
-dat <- readRDS(snakemake@input[["dat"]])
+f_true <- readRDS(snakemake@input[["f_true"]])
 fit <- readRDS(snakemake@input[["fit"]])
 
 
-# For simulations with no factors
-if(is.null(dat$F_mat)){
-    dat$F_mat <- diag(ncol(dat$beta_hat))
-    dat$L_mat_marg <- dat$beta_marg
-    dat$theta_marg <- matrix(0, nrow = nrow(dat$beta_hat), ncol = ncol(dat$beta_hat))
-}
 
 if(snakemake@wildcards[["method"]]  %in%  c("ml", "guide", "factorgo")){
    df <- purrr::map_dfr(seq(length(fit)), function(i){
-                dff <- eval(fit[[i]], dat)
+                dff <- eval(fit[[i]], f_true)
                 dff$subset <- names(fit)[i]
                 dff
                       })
 }else if(is.null(fit$n_factors)){
-    df <- eval(fit, dat)
+    df <- eval(fit, f_true)
     df$subset <- "all"
 }else{
     df <- purrr::map_dfr(1:length(fit$n_factors), function(i){
-                             dff <- eval(fit, dat, n_subset = fit$n_factors[[i]])
+                             dff <- eval(fit, f_true, n_subset = fit$n_factors[[i]])
                              dff$subset <- names(fit$n_factors)[i]
                              dff
                       })
 }
 
+print(df)
+
 df$method <- paste0(snakemake@wildcards[["method"]], "-", snakemake@wildcards[["mtype"]])
 df$scenario <- snakemake@wildcards[["scenario"]]
 df$ndense <- snakemake@wildcards[["ndense"]]
 df$rep <- snakemake@wildcards[["rep"]]
-df$data_file <- snakemake@input[["dat"]]
+df$ftrue_file <- snakemake@input[["f_true"]]
 df$fit_file <- snakemake@input[["fit"]]
 
 saveRDS(df, file = snakemake@output[["out"]])
